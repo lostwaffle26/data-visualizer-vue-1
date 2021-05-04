3a50ae4431f32c529e4e8a4d347d3ddf14592ba2
Tom <tomruiz2611@gmail.com>
2021-05-04 21:50:15 +0200

    add convert csv & redesign & ajout rows

--- src/components/PeopleList.vue
+++ src/components/PeopleList.vue
@@ -1,33 +1,39 @@
 <template>
-  <div class="container">
+  <div>
     <div class="container">
-
       <div class="search">
-        <label>Filter par nom:</label>
-        <input type="text" placeholder="Filter" v-model="filter" />
+        <label>Filter par nom ou prénom:</label>
+        <input type="text" placeholder="Nom ou prénom" v-model="filter" />
       </div>
 
       <div class="pagination">
-        <div>
-          page actuelle : <input v-model="currentPage" type="number"/>
+        <div class="col">
+          <label>Page actuelle : </label>
+          <input v-model="currentPage" type="number" />
         </div>
 
-        <div>
-          Pages à afficher
+        <div class="page">
+          lignes à afficher
           <select v-model="pageSize" placeholder="nombre de lignes à afficher">
             <option>5</option>
             <option>15</option>
             <option>25</option>
             <option>50</option>
           </select>
-          <button @click="prevPage()">Précédant</button>
-          <button @click="nextPage()">Suivant</button>
+          <button @click="prevPage()" class="btn-3">Précédant</button>
+          <button @click="nextPage()" class="btn-3">Suivant</button>
         </div>
-        
       </div>
     </div>
+    <div class="export-field">
+      <button class="btn-3 export-button" @click="saveFile()">
+        Exporter au format CSV
+      </button>
+    </div>
+  </div>
 
-    <table id="table" class="table-fill">
+  <div class="table-style">
+    <table id="table">
       <tr>
         <th>Prénom</th>
         <th>Nom</th>
@@ -37,6 +43,10 @@
         <th>Ville</th>
         <th>Pays</th>
         <th>Téléphone</th>
+        <th>Animal pref</th>
+        <th>fruit pref</th>
+        <th>couleur pref</th>
+        <th>film pref</th>
       </tr>
       <tr v-for="(item, key) in pagedList" :key="key">
         <td>{{ item.firstname }}</td>
@@ -47,6 +57,10 @@
         <td>{{ item.contact.city }}</td>
         <td>{{ item.contact.country }}</td>
         <td>{{ item.contact.phone }}</td>
+        <td>{{ item.preferences.favorite_pet }}</td>
+        <td>{{ item.preferences.favorite_fruit }}</td>
+        <td>{{ item.preferences.favorite_color }}</td>
+        <td>{{ item.preferences.favorite_movie }}</td>
       </tr>
     </table>
   </div>
@@ -88,6 +102,19 @@ export default {
     prevPage() {
       if (this.currentPage > 1) this.currentPage--;
     },
+
+    saveFile() {
+      let dataStr = JSON.stringify({ ...this.paginedList });
+      let dataUri =
+        "data:application/json;charset=utf-8," + encodeURIComponent(dataStr);
+
+      let exportFileName = "list.json";
+
+      let linkElement = document.createElement("a");
+      linkElement.setAttribute("href", dataUri);
+      linkElement.setAttribute("download", exportFileName);
+      linkElement.click();
+    },
   },
   computed: {
     pagedList() {
@@ -114,25 +141,22 @@ export default {
   display: flex;
   flex-direction: column;
   padding: 10px;
-  
 }
 
-
-.pagination{
-  display: flex;
+.pagination {
   padding: 10px;
 }
 
-input{
+input {
   padding: 10px;
 }
 
-select{
+select {
   padding: 10px;
 }
 
-button{
-   background-color: #007acc; /* Green */
+button {
+  background-color: #5200b1;
   border: none;
   color: white;
   padding: 16px 32px;
@@ -144,17 +168,26 @@ button{
   transition-duration: 0.4s;
   cursor: pointer;
   border: 1px solid white;
-
 }
 
-.search{
+.btn-3:hover {
+  background-color: #030303;
+}
+
+.search {
   display: flex;
   padding: 10px;
 }
+
+.table-style {
+  display: flex;
+  justify-content: center;
+}
+
 #table {
   font-family: Arial, Helvetica, sans-serif;
   border-collapse: collapse;
-  width: 100%;
+  width: 90%;
 }
 
 #table td,
@@ -175,7 +208,28 @@ button{
   padding-top: 12px;
   padding-bottom: 12px;
   text-align: left;
-  background-color: #007acc;
+  background: linear-gradient(#5200b1, #120035);
   color: white;
+}
+
+select,
+input {
+  width: 100%;
+  padding: 16px 20px;
+  border: none;
+  border-radius: 4px;
+  background-color: #f1f1f1;
+}
+select {
+  font-size: 15px;
+  width: 10vh;
+}
+
+.col input {
+  width: 80px;
+}
+
+.export-button {
+  margin-bottom: 20px;
 }
 </style>
